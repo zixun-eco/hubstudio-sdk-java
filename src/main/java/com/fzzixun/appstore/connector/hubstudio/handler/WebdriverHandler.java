4fbec376ec450ecb50b3438a936720466519177d
zixun-eco <113669042+zixun-eco@users.noreply.github.com>
2022-10-08 02:25:58 -0400

    feat: 初始化webdriver程序 -1.0.0

--- src/main/java/com/fzzixun/appstore/connector/hubstudio/handler/WebdriverHandler.java
+++ src/main/java/com/fzzixun/appstore/connector/hubstudio/handler/WebdriverHandler.java
@@ -1,18 +1,35 @@
 package com.fzzixun.appstore.connector.hubstudio.handler;
 
 import com.fzzixun.appstore.connector.hubstudio.config.CommandConfig;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 public class WebdriverHandler {
 
+    private static void init() {
+        File file = new File("./chromedriver100.exe");
+        if (!file.exists()) {
+            InputStream inputStream = WebdriverHandler.class.getClassLoader().getResourceAsStream("webdriver/chromedriver100.exe");
+            try {
+                FileUtils.copyInputStreamToFile(inputStream, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
     /**
      * 获取webdriver
      */
     public static ChromeDriver getDriver(int port) {
+        init();
         System.setProperty("webdriver.chrome.driver", CommandConfig.WEBDRIVER_PATH);
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--disable-gpu");
